28c8931a2b575f03bcc793c30601d5052643bfc8
Clinton <61327562+ClintonEziamaka@users.noreply.github.com>
2021-09-06 11:08:44 -0500

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    title: "checkWeathers app",
+    subtitle: "React, React Native",
+    description:
+      "This is a weatherApp project created with React Native and ReactJs that allows users check the weather condition of locations in the United State of America. It collects Cities as text inputs from the users and displays the current weather conditions of that city using API.",
+    image: "./project-1.gif",
+    link: "https://github.com/ClintonEziamaka/checkWeathers",
+  },
+  {
+    title: "Library System",
+    subtitle: "Javascript, Salesforce Lightning and Apex",
+    description:
+      "This is a library system to track books and AV equipments that employees can borrow from a company's library.",
+    image: "./Home-page.png",
+    link: "https://github.com/ClintonEziamaka/Library-System",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "",
+    name: "Jesse Hicks",
+    company: "",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "",
+    company: "",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "Java",
+  "React",
+  "Node.js",
+  "R/Rstudio",
+  "HTML/CSS",
+  "UI/UX design",
+  "SQL",
+  "MongoDB",
+  "Windows OS",
+  "Mac OS"
+];
