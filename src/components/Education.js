0cf807f0b14a39385fc4c1401c30d832ee895574
Clinton Eziamaka <61327562+ClintonEziamaka@users.noreply.github.com>
2023-06-02 21:11:31 +0000

    June2023 Commit

--- src/components/Education.js
+++ src/components/Education.js
@@ -14,7 +14,7 @@ export default function Education() {
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
 {education.map((education) => (
     <div key={education} className="p-2 sm:w-1/2 w-full">
-        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+        <div className="bg-gray-800 rounded flex p-4 h-full ">
             <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
             <span className="text-gray-500 text-sm uppercase">{education.Program}</span>
             <span className="title-font font-medium text-white"> {education.School} </span>
