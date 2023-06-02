5fe2cae59d12138d2cfbaf7e9c1f7b00d8a85a02
Clinton Eziamaka <61327562+ClintonEziamaka@users.noreply.github.com>
2023-06-02 21:24:48 +0000

    June2023 Commit

--- src/components/Education.js
+++ src/components/Education.js
@@ -14,10 +14,12 @@ export default function Education() {
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
 {education.map((education) => (
     <div key={education} className="p-2 sm:w-1/2 w-full">
-        <div className="bg-gray-800 rounded flex p-4 h-full ">
+        <div className="bg-gray-800 rounded flex p-4 h-full <a href="#education" className="mr-5 hover:text-white"> Education </a> ">
             <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+            <span className="flex-grow flex flex-col pl-4">
             <span className="text-gray-500 text-sm uppercase">{education.Program}</span>
             <span className="title-font font-medium text-white"> {education.School} </span>
+            </span>
         </div>
     </div>
 ))} 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,9 @@ export default function Navbar() {
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
             <a href="#skills" className="mr-5 hover:text-white"> Skills</a>
             <a href="#projects" className="mr-5 hover:text-white"> Projects </a>
-            <a href="#certifications" className="mr-5 hover:text-white"> Certifications </a>      
+            <a href="#education" className="mr-5 hover:text-white"> Education </a>
+            <a href="#certifications" className="mr-5 hover:text-white"> Certifications </a>
+            <a href="#" ></>
              </nav>
             <a href="#contact" className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
         Hire Me 
