58f5c9ba2551b78413661bf0e173d9a4f6026e60
Clinton Eziamaka <61327562+ClintonEziamaka@users.noreply.github.com>
2023-06-02 21:26:24 +0000

    June2023 Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,8 +13,7 @@ export default function Navbar() {
             <a href="#projects" className="mr-5 hover:text-white"> Projects </a>
             <a href="#education" className="mr-5 hover:text-white"> Education </a>
             <a href="#certifications" className="mr-5 hover:text-white"> Certifications </a>
-            <a href="#" ></>
-             </nav>
+        </nav>
             <a href="#contact" className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
         Hire Me 
         <ArrowRightIcon className="w-4 h-4 ml-1" />
