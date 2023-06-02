28a717a41447e47527755959ecb6e0eb40b7ba8b
Clinton Eziamaka <61327562+ClintonEziamaka@users.noreply.github.com>
2023-06-02 21:54:06 +0000

    June2023 Commit

--- src/components/Certification.js
+++ src/components/Certification.js
@@ -9,7 +9,7 @@ return(
     <div className="container px-5 py-10 mx-auto text-center">
     <TerminalIcon className="inline-block w-10 text-gray-500 mb-4" />
     <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12"> Certifications </h1>
-    <div className="flex flex-wrap m-4">  
+    <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">  
     {certifications.map((certification) => (
         <div className="p-4 md:w-1/2 w-full">
         <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
