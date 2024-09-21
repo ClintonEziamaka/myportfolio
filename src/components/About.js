78feccdad1c278014dcdc3c7fc70640150c09eae
Clinton Eziamaka <61327562+ClintonEziamaka@users.noreply.github.com>
2024-09-21 03:00:14 +0000

    SeptCommit

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,17 @@ export default function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                    Clinton Eziamaka 
+                    Clinton T. Eziamaka 
                 </h1>
                 <p className="mb-8 leading-relaxed">
-                A Developer with experience designing, developing, and delivering innovative and scalable software solutions and features. 
-                Academically founded in Computer Science with proven track record in determining project requirements and ensuring efficient execution of national and global-scale projects. 
-                Strong background in driving the efficient resolution of technical issues. Demonstrated success in maximizing code quality and conducting unit testing. 
+                Detail-oriented and results-driven software developer with 3 years of experience designing, developing, and delivering scalable software solutions. Proven
+                track record in leading global and national-scale projects, optimizing workflows, and improving team efficiency through automation and data-driven strategies.
+                Adept at collaborating with cross-functional teams to ensure high-quality outcomes and meet tight deadlines. Strong background in Salesforce development,
+                cloud platforms, and public health data analysis, with a focus on ensuring data accuracy and driving actionable insights.
                      </p>
                 <div className="flex justify-center">
-                    <a href="#contact" className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"> Let's work! </a>
-                    <a href="#projects" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"> See previous projects </a>
+                    <a href="#contact" className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"> Let's Work! </a>
+                    <a href="#projects" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"> See Projects </a>
                 </div>
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ export default function Projects() {
             <div className="container px-5 py-10 mx-auto text-center lg:px-40">
                 <div className="flex flex-col w-full mb-20">
                     <DesktopComputerIcon className="mx-auto inline-block w-10 mb-4" />
-                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white"> Project And Works</h1>
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white"> Projects </h1>
                     </div>
                     <div className="flex flex-wrap -m-4">
                         {projects.map((project) => (
